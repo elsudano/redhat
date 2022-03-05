2de89fa5e579885dbf2ed4bfe08c682e992e402a
Carlos de la Torre <carlos.ariel.de.la.torre@gmail.com>
2022-03-05 22:37:34 +0100

    Solve some problems with the name of the functions

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Copyright 2022 Carlos de la Torre. All rights reserved.
+// Use of this source code is governed by a MIT License
+// license that can be found in the LICENSE file.
+package redhat
--- redhat.go
+++ redhat.go
@@ -1,8 +1,3 @@
-/*
-	Copyright 2022 Carlos de la Torre. All rights reserved.
-	Use of this source code is governed by a MIT License
-	license that can be found in the LICENSE file.
-*/
 package redhat
 
 import (
